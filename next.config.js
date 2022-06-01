720777c0580adc1df3abf5d87972e2f7727a27e5
sonac <sonag007@gmail.com>
2022-06-01 20:47:46 +0200

    fix: trying new security headers

--- next.config.js
+++ next.config.js
@@ -1,13 +1,35 @@
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  style-src 'self';
+  img-src * 'self' https://m.media-amazon.com;
+`;
+
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, " ").trim(),
+  },
+];
+
 /** @type {import('next').NextConfig} */
 module.exports = {
-  /*async rewrites() {
+  async rewrites() {
     return [
       {
         source: '/api/:path',
         destination: 'http://localhost:8080/:path',
       }
     ]
-  },*/
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: securityHeaders,
+      },
+    ];
+  },
   reactStrictMode: true,
   experimental: {
     outputStandalone: true,
