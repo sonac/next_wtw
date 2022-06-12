d07a5bdabc72398a145fb1fcdc862deab55332f4
sonac <sonag007@gmail.com>
2022-06-12 13:51:57 +0200

    fix: using next head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
+import NextHead from 'next/head'
 
 import theme  from '../src/theme/index'
 import '../styles/globals.css'
@@ -9,6 +10,9 @@ import '../public/fonts/styles.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
       <ChakraProvider theme={theme}>
+        <NextHead>
+          <meta httpEquiv='Content-Security-Policy' content="default-src '*'" />
+        </NextHead>
         <Component {...pageProps} />
       </ChakraProvider>
   )
