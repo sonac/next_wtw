5f333f6616c646a92e70cfa3acf42c5a25840b50
sonac <sonag007@gmail.com>
2022-06-12 23:18:03 +0200

    fix: remove img

--- src/sections/body.tsx
+++ src/sections/body.tsx
@@ -1,24 +1,17 @@
-import { VStack, Heading, Text } from '@chakra-ui/react';
-import Image from 'next/image'
+import { VStack, Heading, Text } from "@chakra-ui/react";
 
 const imgLoader = ({ src, ...props }: any) => {
-  return src
-}
+  return src;
+};
 
 const body = () => {
   return (
     <VStack>
       <Heading fontSize="6xl">Welcome to WTW</Heading>
-      <Text fontSize='4xl'>Don't know what to watch?</Text>
-      <Text fontSize='4xl'>Don't worry, we got you covered.</Text>
-      <Image 
-        loader={imgLoader}
-        src="https://m.media-amazon.com/images/M/MV5BMzZjNjIxZjUtNzRmYy00MjRjLWI2NjYtNTc0ZjdmZGRlNDI0XkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg" 
-        width="100%"
-        height="100%"
-        />
+      <Text fontSize="4xl">Don't know what to watch?</Text>
+      <Text fontSize="4xl">Don't worry, we got you covered.</Text>
     </VStack>
-  )
-}
+  );
+};
 
 export default body;
