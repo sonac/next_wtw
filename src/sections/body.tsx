abc99a86345e4f538e75a3d7e4e0ff075b85682e
sonac <sonag007@gmail.com>
2022-06-12 14:52:05 +0200

    fix: test2

--- src/sections/body.tsx
+++ src/sections/body.tsx
@@ -1,6 +1,10 @@
 import { VStack, Heading, Text } from '@chakra-ui/react';
 import Image from 'next/image'
 
+const imgLoader = ({ src, ...props }: any) => {
+  return src
+}
+
 const body = () => {
   return (
     <VStack>
@@ -8,6 +12,7 @@ const body = () => {
       <Text fontSize='4xl'>Don't know what to watch?</Text>
       <Text fontSize='4xl'>Don't worry, we got you covered.</Text>
       <Image 
+        loader={imgLoader}
         src="https://m.media-amazon.com/images/M/MV5BMzZjNjIxZjUtNzRmYy00MjRjLWI2NjYtNTc0ZjdmZGRlNDI0XkEyXkFqcGdeQXVyMTA4NjE0NjEy._V1_.jpg" 
         width="100%"
         height="100%"
