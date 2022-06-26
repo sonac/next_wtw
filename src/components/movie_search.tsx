96ad99dfe323b7f74dad80663826094663b7c081
sonac <sonag007@gmail.com>
2022-06-26 12:48:37 +0200

    feat: splitting search to local and global

--- src/components/movie_search.tsx
+++ src/components/movie_search.tsx
@@ -29,19 +29,31 @@ const MovieSearch: React.FC<SeachProps> = ({ isOpen, onClose, setClickedMovie, s
     const [loading, setLoading] = useState(false);
     const [input, setInput] = useState('');
 
-    const search = async (e: any) => {
+    const searchLocal = async (e: any) => {
         if (e.key == 'Enter') {
-            setLoading(true);
-            const resp = await fetch(`/api/search`, 
+            setLoading(true)
+            const resp = await fetch(`/api/search-local`, 
                 {
                     method: 'POST',
                     body: input
                 }
             )
-            console.log(resp);
             setMovies(await resp.json());
             setLoading(false);
         }
+    }
+
+    const search = async (e: any) => {
+        setLoading(true);
+        const resp = await fetch(`/api/search`, 
+            {
+                method: 'POST',
+                body: input
+            }
+        )
+        console.log(resp);
+        setMovies(await resp.json());
+        setLoading(false);
     }
 
     const clickMovie = (m: ImdbMovie): void => {
@@ -58,14 +70,21 @@ const MovieSearch: React.FC<SeachProps> = ({ isOpen, onClose, setClickedMovie, s
             <ModalHeader>
                 <Input placeholder='movie name...' size='lg' variant='unstyled'
                     onChange={e => setInput(e.target.value)}
-                    onKeyDown={search}
+                    onKeyDown={searchLocal}
                 />
             </ModalHeader>
             <ModalCloseButton />
             <ModalBody bg="rgba(86, 86, 86, 0.1)">
-                {loading ? <Spinner /> : movies.map(m => <Text key={m.title} onClick={() => clickMovie(m)}
+                {loading ? 
+                    <Spinner /> : 
+                    <div>{movies != null ? movies.map(m => <Text key={m.title} onClick={() => clickMovie(m)}
                         _hover={{cursor: 'pointer', bg: 'rgba(86, 86, 86, 1)'}}
-                    >{m['year']} {m['title']}</Text>)}
+                    >{m['year']} {m['title']}</Text>) : <></>}
+                    <Text key="globalSearch" onClick={search} _hover={{cursor: 'pointer', bg: 'rgba(86, 86, 86, 1)'}}>
+                        ...
+                    </Text>
+                    </div>
+                }
             </ModalBody>
             </ModalContent>
         </Modal>
