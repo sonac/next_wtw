09c85edc2e40e561b63468c96ed962b2c625feb2
sonac <sonag007@gmail.com>
2024-01-07 23:34:35 +0100

    feat: adding 2024 year tab

--- pages/animes/[user_id].tsx
+++ pages/animes/[user_id].tsx
@@ -1,27 +1,36 @@
 import useSWR from "swr";
 import { useState, useEffect } from "react";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
-import  { UserTitle } from "../../src/components/title";
+import { UserTitle } from "../../src/components/title";
 import UserTitles from "../../src/components/user_titles";
-import { Tab, TabList, TabPanel, TabPanels, Tabs, VStack } from "@chakra-ui/react";
+import {
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  VStack,
+} from "@chakra-ui/react";
 import Header from "../../src/sections/header";
 
-const animesFetcher = () => 
-  fetch(`/api/user-animes`, { credentials: "include" }).then((res) => res.json());
+const animesFetcher = () =>
+  fetch(`/api/user-animes`, { credentials: "include" }).then((res) =>
+    res.json()
+  );
 
 const UserAnimes = () => {
   const [animes, setAnimes] = useState<UserTitle[]>([]);
-  const {data, error} = useSWR("seenAnimes", animesFetcher);
+  const { data, error } = useSWR("seenAnimes", animesFetcher);
   const router = useRouter();
 
   useEffect(() => {
-      if (error) {
-        router.push("/")
-      }
-    [router]
+    if (error) {
+      router.push("/");
+    }
+    [router];
   });
-    
+
   if (error) {
     return <div>Redirecting...</div>;
   }
@@ -31,7 +40,7 @@ const UserAnimes = () => {
   }
 
   return (
-    <VStack 
+    <VStack
       h={{ md: "100vh" }}
       w={{ md: "100%" }}
       p={0}
@@ -48,28 +57,59 @@ const UserAnimes = () => {
         </TabList>
         <TabPanels>
           <TabPanel>
-            <UserTitles titles={[]} endpoint="anime"  isDiscovery={false}/>
+            <UserTitles titles={[]} endpoint="anime" isDiscovery={false} />
           </TabPanel>
           <TabPanel>
-            <UserTitles titles={animes ? animes.filter(a => !a.isFinished) : []} endpoint="anime"  isDiscovery={false}/>
+            <UserTitles
+              titles={animes ? animes.filter((a) => !a.isFinished) : []}
+              endpoint="anime"
+              isDiscovery={false}
+            />
           </TabPanel>
           <TabPanel>
             <Tabs>
               <TabList justifyContent="space-evenly">
+                <Tab fontSize="2em">2024</Tab>
                 <Tab fontSize="2em">2023</Tab>
               </TabList>
               <TabPanels>
                 <TabPanel>
-                  <UserTitles titles={animes ? animes.filter(a => a.isFinished && new Date(a.dateFinished).getFullYear() === 2023) : []} endpoint="anime"  isDiscovery={false}/>
+                  <UserTitles
+                    titles={
+                      animes
+                        ? animes.filter(
+                            (a) =>
+                              a.isFinished &&
+                              new Date(a.dateFinished).getFullYear() === 2024
+                          )
+                        : []
+                    }
+                    endpoint="anime"
+                    isDiscovery={false}
+                  />
+                </TabPanel>
+                <TabPanel>
+                  <UserTitles
+                    titles={
+                      animes
+                        ? animes.filter(
+                            (a) =>
+                              a.isFinished &&
+                              new Date(a.dateFinished).getFullYear() === 2023
+                          )
+                        : []
+                    }
+                    endpoint="anime"
+                    isDiscovery={false}
+                  />
                 </TabPanel>
               </TabPanels>
             </Tabs>
           </TabPanel>
         </TabPanels>
       </Tabs>
-
     </VStack>
-  )
-} 
+  );
+};
 
 export default UserAnimes;
--- pages/books/[user_id].tsx
+++ pages/books/[user_id].tsx
@@ -1,28 +1,37 @@
 import useSWR from "swr";
 import { useState, useEffect } from "react";
-import { useRouter } from 'next/router';
+import { useRouter } from "next/router";
 
-import  { UserTitle } from "../../src/components/title";
+import { UserTitle } from "../../src/components/title";
 import UserTitles from "../../src/components/user_titles";
-import { Tab, TabList, TabPanel, TabPanels, Tabs, VStack } from "@chakra-ui/react";
+import {
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  VStack,
+} from "@chakra-ui/react";
 import Header from "../../src/sections/header";
 
-const booksFetcher = () => 
-  fetch(`/api/user-books`, { credentials: "include" }).then((res) => res.json());
+const booksFetcher = () =>
+  fetch(`/api/user-books`, { credentials: "include" }).then((res) =>
+    res.json()
+  );
 
 const UserBooks = () => {
   const [books, setBooks] = useState<UserTitle[]>([]);
-  const {data, error} = useSWR("seenBooks", booksFetcher);
+  const { data, error } = useSWR("seenBooks", booksFetcher);
 
   const router = useRouter();
 
   useEffect(() => {
-      if (error) {
-        router.push("/")
-      }
-    [router]
+    if (error) {
+      router.push("/");
+    }
+    [router];
   });
-    
+
   if (error) {
     return <div>Redirecting...</div>;
   }
@@ -32,7 +41,7 @@ const UserBooks = () => {
   }
 
   return (
-    <VStack 
+    <VStack
       h={{ md: "100vh" }}
       w={{ md: "100%" }}
       p={0}
@@ -49,28 +58,59 @@ const UserBooks = () => {
         </TabList>
         <TabPanels>
           <TabPanel>
-            <UserTitles titles={[]} endpoint="book" isDiscovery={false}/>
+            <UserTitles titles={[]} endpoint="book" isDiscovery={false} />
           </TabPanel>
           <TabPanel>
-            <UserTitles titles={books ? books.filter(b => !b.isFinished) : []} endpoint="book" isDiscovery={false}/>
+            <UserTitles
+              titles={books ? books.filter((b) => !b.isFinished) : []}
+              endpoint="book"
+              isDiscovery={false}
+            />
           </TabPanel>
           <TabPanel>
             <Tabs>
               <TabList justifyContent="space-evenly">
+                <Tab fontSize="2em">2024</Tab>
                 <Tab fontSize="2em">2023</Tab>
               </TabList>
               <TabPanels>
                 <TabPanel>
-                  <UserTitles titles={books ? books.filter(b => b.isFinished && new Date(b.dateFinished).getFullYear() === 2023) : []} endpoint="book" isDiscovery={false}/>
+                  <UserTitles
+                    titles={
+                      books
+                        ? books.filter(
+                            (b) =>
+                              b.isFinished &&
+                              new Date(b.dateFinished).getFullYear() === 2024
+                          )
+                        : []
+                    }
+                    endpoint="book"
+                    isDiscovery={false}
+                  />
+                </TabPanel>
+                <TabPanel>
+                  <UserTitles
+                    titles={
+                      books
+                        ? books.filter(
+                            (b) =>
+                              b.isFinished &&
+                              new Date(b.dateFinished).getFullYear() === 2023
+                          )
+                        : []
+                    }
+                    endpoint="book"
+                    isDiscovery={false}
+                  />
                 </TabPanel>
               </TabPanels>
             </Tabs>
           </TabPanel>
         </TabPanels>
       </Tabs>
-
     </VStack>
-  )
-} 
+  );
+};
 
 export default UserBooks;
