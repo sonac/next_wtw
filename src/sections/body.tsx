6652c68c2a56bc59759ea77c24cce0deaf13509b
sonac <sonag007@gmail.com>
2024-10-06 00:59:59 +0200

    feat: better landos

--- src/sections/body.tsx
+++ src/sections/body.tsx
@@ -1,13 +1,67 @@
-import { VStack, Heading, Text } from "@chakra-ui/react";
+import { VStack, Heading, Text, Box, SimpleGrid, Flex } from "@chakra-ui/react";
+import SectionCard from "../components/section_card";
+
+interface Section {
+  name: string;
+  imageUrl: string;
+  link: string;
+}
+
+const sections: Section[] = [
+  {
+    name: "Movies",
+    imageUrl: "/movies.webp",
+    link: "/movies",
+  },
+  {
+    name: "Series",
+    imageUrl: "/series.webp",
+    link: "/series",
+  },
+  {
+    name: "Games",
+    imageUrl: "/games.webp",
+    link: "/games",
+  },
+  {
+    name: "Books",
+    imageUrl: "/books.webp",
+    link: "/books",
+  },
+  {
+    name: "Animes",
+    imageUrl: "/animes.webp",
+    link: "/animes",
+  },
+];
 
 const body = () => {
+  console.log(sections);
   return (
-    <VStack pt={"40%"}>
-      <Heading fontSize="6xl">Welcome to MyList</Heading>
-      <Text fontSize="4xl">Here you can track everything</Text>
-      <Text fontSize="4xl">All movies that you've wathced</Text>
-      <Text fontSize="4xl">All TV Series that you've binged</Text>
-    </VStack>
+    <Box p={8} mt="70px">
+      <Heading mb={8} textAlign="center" fontSize="4xl">
+        Welcome to My List (the best one)
+      </Heading>
+      <Flex
+        flexWrap="wrap"
+        justify="space-around"
+        align="center"
+        gap={8}
+        w="75vw"
+        mx="auto"
+      >
+        {sections.map((section) => {
+          return (
+            <SectionCard
+              key={section.name}
+              name={section.name}
+              imageUrl={section.imageUrl}
+              link={section.link}
+            />
+          );
+        })}
+      </Flex>
+    </Box>
   );
 };
 
